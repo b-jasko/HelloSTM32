b4577ba442ba93e1bcf1bc0eaded6e062d3fab6a
b-jasko <bjasko97@gmail.com>
2018-07-08 20:51:16 +0200

    zad1

--- src/main.c
+++ src/main.c
@@ -10,13 +10,10 @@
 
 
 #include "stm32f10x.h"
-			
-//void delay(int time) {
-//	int i;
-//	for (i = 0; i < time*4000; i++); //4000 dobrane eksperymentalnie, aby opóŸnienie by³o w ms
-//}
 
 volatile uint32_t timer_ms = 0;
+volatile uint32_t flag = 0;
+volatile uint32_t led = 0;
 
 void SysTick_Handler() { //obs³uga przerwania od timera SysTick
 	if (timer_ms) {
@@ -29,35 +26,75 @@ void delay_ms(int time) {
 	while (timer_ms);
 }
 
+void EXTI15_10_IRQHandler()
+{
+    if (EXTI_GetITStatus(EXTI_Line13)) {
+        if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == 0) {
+            flag = !flag;
+            GPIO_SetBits(GPIOA,GPIO_Pin_5);
+        } else {
+            GPIO_ResetBits(GPIOA,GPIO_Pin_5);
+        }
+
+        EXTI_ClearITPendingBit(EXTI_Line13);
+    }
+}
+
+void zad1() {
+	GPIO_SetBits(GPIOC, 1<<led);
+	delay_ms(100);
+	GPIO_ResetBits(GPIOC, 1<<led);
+	if (flag == 0) {
+		if (++led >= 10) {
+			led = 0;
+		}
+	} else {
+		if (--led <= 0) {
+			led = 9;
+		}
+	}
+}
+
 int main(void)
 {
 
-	GPIO_InitTypeDef gpio; //obiekt gpio bêd¹cy konfiguracj¹ portów GPIO
+	GPIO_InitTypeDef gpio;
+	EXTI_InitTypeDef exti;
+	NVIC_InitTypeDef nvic;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC, ENABLE); //pod³¹czenie zagara do portu A oraz C
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE); //pod³¹czenie zagara
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //modu³ funkcji alternatywnych
 
-	//ustawienie PA5 jako wyjœcie
-	GPIO_StructInit(&gpio); //domyœlna konfiguracja
-	gpio.GPIO_Pin = GPIO_Pin_5; // konfiguracja pinu 5
-	gpio.GPIO_Mode = GPIO_Mode_Out_PP; // jako wyjœcie push-pull
-	GPIO_Init(GPIOA, &gpio); //inicjalizacja modu³u GPIOA
+	GPIO_StructInit(&gpio);
+	gpio.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjœcie push-pull
+	GPIO_Init(GPIOC, &gpio);
 
-	//ustawienie PC13 jako wejœcie
+	gpio.GPIO_Pin = GPIO_Pin_5;
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjœcie push-pull
+	GPIO_Init(GPIOA, &gpio);
+
 	gpio.GPIO_Pin = GPIO_Pin_13;
 	gpio.GPIO_Mode = GPIO_Mode_IPU; //wejœcie z rezystorem pull-up
 	GPIO_Init(GPIOC, &gpio);
 
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource13);
+
+	EXTI_StructInit(&exti);
+	exti.EXTI_Line = EXTI_Line13;
+	exti.EXTI_Mode = EXTI_Mode_Interrupt;
+	exti.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	exti.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&exti);
+
+	nvic.NVIC_IRQChannel = EXTI15_10_IRQn;
+	nvic.NVIC_IRQChannelPreemptionPriority = 0x00;
+	nvic.NVIC_IRQChannelSubPriority = 0x00;
+	nvic.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&nvic);
+
 	SysTick_Config(SystemCoreClock / 1000); //przerwanie od timera SysTick co 1ms
-
 	while(1) {
-		if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == 0) { //jeœli przycisk wciœniêty
-			GPIO_SetBits(GPIOA, GPIO_Pin_5); //zapalenie diody LD2
-			delay_ms(100);
-			GPIO_ResetBits(GPIOA, GPIO_Pin_5); //zgaszenie diody LD2
-			delay_ms(100);
-		} else {
-			GPIO_ResetBits(GPIOA, GPIO_Pin_5);
-		}
-
+		zad1();
 	}
 }
