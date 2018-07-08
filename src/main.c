1f072faa8471f8e914f655206d8d280062ca3eb3
b-jasko <bjasko97@gmail.com>
2018-07-09 01:08:10 +0200

    zad2

--- src/main.c
+++ src/main.c
@@ -13,9 +13,10 @@
 
 volatile uint32_t timer_ms = 0;
 volatile uint32_t flag = 0;
+volatile uint32_t isKeyDown = 0;
 volatile uint32_t led = 0;
 
-void SysTick_Handler() { //obs³uga przerwania od timera SysTick
+void SysTick_Handler() { //obsluga przerwania od timera SysTick
 	if (timer_ms) {
 		timer_ms--; //co 1ms dekrementuj timer_ms
 	}
@@ -31,8 +32,10 @@ void EXTI15_10_IRQHandler()
     if (EXTI_GetITStatus(EXTI_Line13)) {
         if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == 0) {
             flag = !flag;
+            isKeyDown = 1;
             GPIO_SetBits(GPIOA,GPIO_Pin_5);
         } else {
+        	isKeyDown = 0;
             GPIO_ResetBits(GPIOA,GPIO_Pin_5);
         }
 
@@ -55,6 +58,17 @@ void zad1() {
 	}
 }
 
+void zad2() {
+	GPIO_SetBits(GPIOC, 1<<led);
+	if (isKeyDown) {
+		isKeyDown = 0;
+		GPIO_ResetBits(GPIOC, 1<<led);
+		if (++led >= 10) {
+			led = 0;
+		}
+	}
+}
+
 int main(void)
 {
 
@@ -62,20 +76,20 @@ int main(void)
 	EXTI_InitTypeDef exti;
 	NVIC_InitTypeDef nvic;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE); //pod³¹czenie zagara
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //modu³ funkcji alternatywnych
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE); //podlaczenie zagara
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE); //modul funkcji alternatywnych
 
 	GPIO_StructInit(&gpio);
 	gpio.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;
-	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjœcie push-pull
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjscie push-pull
 	GPIO_Init(GPIOC, &gpio);
 
 	gpio.GPIO_Pin = GPIO_Pin_5;
-	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjœcie push-pull
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP; //wyjscie push-pull
 	GPIO_Init(GPIOA, &gpio);
 
 	gpio.GPIO_Pin = GPIO_Pin_13;
-	gpio.GPIO_Mode = GPIO_Mode_IPU; //wejœcie z rezystorem pull-up
+	gpio.GPIO_Mode = GPIO_Mode_IPU; //wejscie z rezystorem pull-up
 	GPIO_Init(GPIOC, &gpio);
 
 	GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource13);
@@ -94,7 +108,8 @@ int main(void)
 	NVIC_Init(&nvic);
 
 	SysTick_Config(SystemCoreClock / 1000); //przerwanie od timera SysTick co 1ms
+
 	while(1) {
-		zad1();
+		zad2();
 	}
 }
